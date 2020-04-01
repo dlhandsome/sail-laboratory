ee9c00b474a8205554c68ed2e45060c8db4ddbb6
dlhandsome <505441575@qq.com>
2020-04-01 14:29:01 +0800

    fix: fixed image links

--- miniprogram/pages/we-cropper/dashboard/index.js
+++ miniprogram/pages/we-cropper/dashboard/index.js
@@ -3,7 +3,7 @@ import list from '../config/menu'
 Page({
   data: {
     list,
-    logo: 'https://act.weixin.qq.com/static/images/202003/43c73c13fa0c0393ede58b2d25655991.jpg',
+    logo: '/images/we-cropper.jpg',
     title: 'we-cropper',
     desc: '一款灵活小巧的 canvas 裁剪器'
   }
--- miniprogram/pages/we-debug/dashboard/index.js
+++ miniprogram/pages/we-debug/dashboard/index.js
@@ -1,10 +1,7 @@
-import list from '../config/menu'
 import weDebug from '../../../utils/debug'
 
 Page({
   data: {
-    list,
-    logo: 'https://act.weixin.qq.com/static/images/202003/43c73c13fa0c0393ede58b2d25655991.jpg',
     title: 'we-debug',
     desc: '一款灵活、易拓展的小程序调试工具'
   },
